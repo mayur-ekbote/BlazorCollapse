7ca4733f476dda0e12a7dad7b848d001e89d448a
Mayur Ekbote <mayur@fluidesk.com>
2024-02-17 00:12:48 +0530

    Add project files.

---
+++ Shared/SimpleCollapse/CollapseState.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorCollapse.Shared.SimpleCollapse;
+
+public class CollapseState
+{
+    public bool IsCollapsed { get; set; } = true;
+    public Action<object>? OnToggle {  get; set; }
+    public void Collapse(object sender)
+    {
+        IsCollapsed = true;
+        OnToggle?.Invoke(sender);
+    }
+    public void Exapand(object sender) 
+    {
+        IsCollapsed = false;
+        OnToggle?.Invoke(sender);
+    }
+    public void Toggle(object sender)
+    {
+        IsCollapsed = !IsCollapsed;
+        OnToggle?.Invoke(sender);
+    }
+}
